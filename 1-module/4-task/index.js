f2735458f8f8626546b0434ac2fd4b10bb4cb249
aksiniya <aksiiniya@gmail.com>
2021-08-11 14:28:13 +0300

    index.js

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,12 @@
 function checkSpam(str) {
-  // ваш код...
+  /*Чтобы проверка была нечувствительна к регистру, сначала мы приводим проверочные слова к нижнему регистру,
+  а затем и саму строку, которую проверяем*/
+  let xSpam = 'XXX'.toLowerCase();
+  let betSpam = '1xBet'.toLowerCase();
+
+  if (str.toLowerCase().includes(xSpam) === true || str.toLowerCase().includes(betSpam) === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
