af7ce34f3988d3ecc5f791a32e94a35088b3add3
aksiniya <aksiiniya@gmail.com>
2021-08-18 18:23:16 +0300

    sumSalary with 2 solutions

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,45 @@
+/*
+//*FIRST VERSION OF SOLUTION 
+
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+
+  for (let key in salaries) {
+    if (typeof salaries[key] === 'number' &&
+      typeof salaries[key] !== 'string' &&
+      typeof salaries[key] !== 'object' &&
+      typeof salaries[key] !== 'undefined' &&
+      typeof salaries[key] !== 'boolean' &&
+      isNaN(salaries[key]) === false &&
+      isFinite(salaries[key]) === true) {
+      sum += salaries[key];
+    } else if (Object.keys(salaries).length === 0) {
+      return 0;
+    }
+  }
+  return sum;
+}*/
+
+//*SECOND VERSION OF SOLUTION
+
+function sumSalary(salaries) {
+  let sum = 0;
+  let values = Object.values(salaries);
+
+  for (let i in values) {
+
+    if (typeof values[i] === 'number' &&
+      typeof values[i] !== 'string' &&
+      typeof values[i] !== 'boolean' &&
+      isNaN(values[i]) === false &&
+      isFinite(values[i]) === true) {
+
+      sum += values[i];
+
+    } else if (Object.values(salaries).length === 0) {
+
+      return 0;
+    }
+  }
+  return sum;
 }
