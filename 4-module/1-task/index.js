8c1776d5200a9ba36523b7d7a1378ee10eacffaf
aksiniya <aksiiniya@gmail.com>
2021-08-25 21:37:37 +0300

    makeFriendsList function

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,17 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  //*create element ul
+  let ul = document.createElement("UL");
+
+  //*create loop cause we need three li
+  for (let i = 0; i < 3; i++) {
+    //*create element li
+    let li = document.createElement("LI");
+    //*li text should be friends[index].firstName + " " + friends[index].lastName
+    li.textContent = friends[i].firstName + " " + friends[i].lastName;
+    //*append li in ul
+    ul.appendChild(li);
+  }
+
+  //*Return DOM element UL with appended li in it 
+  return ul;
 }
