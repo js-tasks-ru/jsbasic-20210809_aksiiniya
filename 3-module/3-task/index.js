51074fefb53aaaa9b8c7fa6b0c93618c61c4146b
aksiniya <aksiiniya@gmail.com>
2021-08-25 18:03:12 +0300

    camelize str function

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,18 @@
 function camelize(str) {
-  // ваш код...
+  //*split str to arr of symbols separated by comma 
+  let arr = str.split('');
+
+  let transformedARR = arr.map(function (item, index, array) {
+    if (item === '-') {
+      //*next item after '-' becomes uppercase 
+      //*index - index of item, array[index + ] => item with special index, we  need to UpperCase this item
+      array[index + 1] = array[index + 1].toUpperCase();
+
+    } else {
+      return item;
+    }
+  });
+
+  return transformedARR.join('');
+
 }
