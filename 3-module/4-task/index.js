e8da9fed9be424e71878adb78d19af74f309799b
aksiniya <aksiiniya@gmail.com>
2021-08-25 18:52:18 +0300

    showSalary function

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,13 @@
 function showSalary(users, age) {
-  // ваш код...
+  //*create array that contains all items that fit age 
+  let arr = users.filter((item, index, arr) => {
+    return item.age <= age;
+  });
+  //*return name and balance from that array
+  let string = arr.map((item, index, arr) => {
+    return `${item.name}, ${item.balance}`;
+  });
+  //*add join by line translation
+  return string.join('\n');
+
 }
